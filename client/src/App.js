3a1442896c2ba9b47eb0e1e1c726a423d849e546
Ryan Goderis <rgoderis789@gmail.com>
2020-07-20 14:34:33 -0400

    added components of ImageDisplay and ImageSearch

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 
+// Flickr api 
+// key: 558d44c427e351de9040cd0eb74be930
+// secret: 31b0e5f1d3c94cfb
+
 function App() {
   return (
     <div className="App">
+      <h1>Blue Toad Sample Project</h1>
 
     </div>
   );
