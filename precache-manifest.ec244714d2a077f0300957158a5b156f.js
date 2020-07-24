af37a990249629374a6f00b4ff991ee285225676
Ryan Goderis <rgoderis789@gmail.com>
2020-07-24 13:01:43 -0400

    Updates

---
+++ precache-manifest.ec244714d2a077f0300957158a5b156f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a8132d5cbfd8159a0493653b957bb5",
+    "url": "/BlueToad-ScreeningProject/index.html"
+  },
+  {
+    "revision": "7d33447b4bf9231da370",
+    "url": "/BlueToad-ScreeningProject/static/css/main.cdc9ee55.chunk.css"
+  },
+  {
+    "revision": "ce6861de32294eaaf0f5",
+    "url": "/BlueToad-ScreeningProject/static/js/2.08a89009.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BlueToad-ScreeningProject/static/js/2.08a89009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d33447b4bf9231da370",
+    "url": "/BlueToad-ScreeningProject/static/js/main.c7cf5791.chunk.js"
+  },
+  {
+    "revision": "1c11f0a9fe8c1b6e2447",
+    "url": "/BlueToad-ScreeningProject/static/js/runtime-main.ca21337a.js"
+  }
+]);
